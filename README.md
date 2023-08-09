1aa325618ecd51fce5bbcae54c98b1269c952b19
angelnavas <angel.navas.diaz@gmail.com>
2023-08-09 07:56:18 +0200

    Solution Proposal
    Added minimum functions to enable todos download ( REST ) and saving to Separated CSV files at folder.
    Check README.md for rationale

--- README.md
+++ README.md
@@ -10,3 +10,19 @@
 
 ## Extra points
 - Use _requests_ library from [PyPI](https://pypi.org/project/requests/)
+
+
+# Angel Navas - Solution proposal
+
+## Rationale 
+In this example the above rules have been followed, creating multiple csv files from individual "alls" present in the REST request response.
+Being an exercise, the use of an API service has been simplified, integrating all its functions within ApiService.
+In a real case, the separation of functionalities, and given that these services can evolve, a greater separation should be chosen, with the part of creating files in App.py and leaving ApiService only for the download of the REST request.
+
+
+
+## Possible improvements
+- Add user id to file name for better identification.
+- Load the received data to a database, for a better performance in intensive use of resources.
+- Better handling of errors in case the folder does not exist or the data received is not correct or does not exist.
+-Create unit tests to detect possible failures automatically.
--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -1,11 +1,49 @@
 from sys import stderr
-
+import requests
+import datetime
 
 class ApiService:
+    source_url = "https://jsonplaceholder.typicode.com/todos/"
+    storage_folder = "storage"
+    todos = {}
+
     def __init__(self):
         pass
 
+    def get_todos(self, source_url):
+        """
+        Gets all TODOs from source_url
+
+        :return: json
+        """
+        print('Todos request', file=stderr)
+        response = requests.get(source_url)
+        if response.status_code == 200:
+            print('Request OK', file=stderr)
+            return response.json()
+        else:
+            print('Request NOT OK', file=stderr)
+            return None
+
+    def todos_to_csv(self, todos, storage_folder):
+        """
+        Saves the TODOs to the storage folder with CSV format (one file per row)
+
+        :param todos:
+        :param storage_folder:
+        :return:
+        """
+        for todo in todos:
+            filename = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{todo['id']}.csv"
+            with open(f"{storage_folder}/{filename}", "w") as f:
+                f.write("userId,id,title,completed\n")
+                f.write(f"{todo['userId']},{todo['id']},{todo['title']},{todo['completed']}\n")
+
     def run(self):
         print('Running ApiService', file=stderr)
-
         # TODO: follow README.md instructions
+
+        self.todos = self.get_todos(self.source_url)
+        self.todos_to_csv(self.todos, self.storage_folder)
+
+        print('Finished ApiService. CSV Files saved to storage folder.', file=stderr)
